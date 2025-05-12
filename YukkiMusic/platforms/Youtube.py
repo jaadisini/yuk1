09d6cbe731ca7142839bf0fcb04079887a08b1f4
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-12 18:25:13 +0800

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -16,7 +16,7 @@ from async_lru import alru_cache
 from py_yt import VideosSearch
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from yt_dlp import YoutubeDL
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from YukkiMusic.utils.database import is_on_off
